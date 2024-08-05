b441e148ea037ccc96cf65309dd149dce8c04765
Xrash <natnaelbelayteklemariam@gmail.com>
2024-08-05 20:59:04 +0300

    Code for the onboarding task 2

---
+++ on-boarding/product-2/product2.dart
@@ -0,0 +1,128 @@
+import "dart:io";
+
+void main() {
+  String? prompt;
+  ProductManger man = ProductManger();
+  var prompts = {
+    "add": man.addProduct,
+    "delete": man.deleteProduct,
+    "view": man.view,
+    "view all": man.viewALL,
+    "edit": man.editProduct
+  };
+  while (prompt != "exit") {
+    stdout.write('>> ');
+    prompt = stdin.readLineSync() ?? "";
+    if (prompts.containsKey(prompt)) {
+      prompts[prompt]?.call();
+    } else if (prompt != "exit") {
+      print("Unknown Command");
+    }
+  }
+}
+
+class Product {
+  String? name, disc;
+  int? price;
+  Product(String? name, disc, int price) {
+    this.name = name;
+    this.disc = disc;
+    this.price = price;
+  }
+  String? getName() {
+    return name;
+  }
+
+  String? getDisc() {
+    return disc;
+  }
+
+  int? getPrice() {
+    return price;
+  }
+}
+
+//add, view all, view, edit a product, delete a product
+class ProductManger {
+  var allProducts;
+  ProductManger() {
+    allProducts = {};
+  }
+  int addProduct() {
+    stdout.write("Enter name: ");
+    var name = stdin.readLineSync();
+    if (name == "") {
+      print("Invalid Input");
+      return 0;
+    }
+    stdout.write("Enter Description: ");
+    var temp = stdin.readLineSync();
+    if (temp == null) {
+      print("Invalid Input");
+      return 0;
+    }
+    stdout.write("Enter Price: ");
+    int price;
+    try {
+      price = int.parse(stdin.readLineSync() ?? "");
+    } catch (e) {
+      print("Invalid Input");
+      return 0;
+    }
+    allProducts[name] = Product(name, temp, price);
+    return 1;
+  }
+
+  void view() {
+    stdout.write("Enter Name: ");
+    String? name = stdin.readLineSync();
+    if (name == null) {
+      print("Invalid name");
+      return;
+    }
+    if (allProducts.containsKey(name)) {
+      print(
+          "Product Name: $name\n Product Discription: ${allProducts[name].getDisc()}\n Price: ${allProducts[name].getPrice()}");
+    } else {
+      print("No such product");
+    }
+  }
+
+  void viewALL() {
+    for (final name in allProducts.keys) {
+      print(
+          "Product Name: $name\n Product Discription: ${allProducts[name].getDisc()}\n Price: ${allProducts[name].getPrice()}");
+    }
+  }
+
+  void deleteProduct() {
+    stdout.write("Enter Name: ");
+    String? name = stdin.readLineSync();
+    if (name == "") {
+      print("Invalid Input");
+      return;
+    }
+    if (allProducts.containsKey(name)) {
+      allProducts.remove(name);
+      print("Successfully removed $name");
+    } else {
+      print("No such product");
+    }
+  }
+
+  void editProduct() {
+    stdout.write("Enter Name: ");
+    String? name = stdin.readLineSync();
+    if (name == "") {
+      print("Invalid Input");
+      return;
+    }
+    if (allProducts.containsKey(name)) {
+      if (addProduct() == 1) {
+        allProducts.remove(name);
+      }
+    } else {
+      print("No such product");
+    }
+  }
+}
